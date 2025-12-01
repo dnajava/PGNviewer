130fcd2257e6aaa59fdb51a28e1f0b4d3f1e9226
Ilpo Kantonen <ilkant@users.noreply.github.com>
2025-12-01 05:11:09 +0200

    Add files via upload

--- gameheaders.py
+++ gameheaders.py
@@ -1,14 +1,14 @@
 class GameHeaders:
     """PGN-tiedoston header-osio (kaikki standardit tagit)"""
-    event: str = "?"
-    site: str = "?"
-    date: Optional[date] = None
-    round: str = "?"
-    white: Player = field(default_factory=Player)
-    black: Player = Player(name="?")
-    result: Result = Result.ONGOING
-    eco: Optional[str] = None
-    opening: Optional[str] = None
-    variation: Optional[str] = None
+    event:      str = "?"
+    site:       str = "?"
+    date:       Optional[date] = None
+    round:      str = "?"
+    white:      Player = field(default_factory=Player)
+    black:      Player = Player(name="?")
+    result:     Result = Result.ONGOING
+    eco:        Optional[str] = None
+    opening:    Optional[str] = None
+    variation:  Optional[str] = None
 
     # Lisää tarvittaessa: annotator, plycount terminaton jne.
