3de6824d40aa168109e4329bb30d832f30305c04
Ilpo Kantonen <ilkant@users.noreply.github.com>
2025-12-01 04:46:10 +0200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QSplitter, QTextEdit,
+    QLabel, QVBoxLayout, QWidget, QPushButton, QHBoxLayout
+)
+from ui import PGNViewerWindow
+from pgn_viewer2 import ChessGame
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # Esimerkki: ladataan PGN-tiedosto
+
+    with open("/mnt/sda3/bigs/PGN/_pgn_lauttssk_JSM-2025.pgn") as f:
+        pgn_text = f.read()
+
+    game = ChessGame.from_pgn_string(pgn_text)
+    window = PGNViewerWindow(game)
+    window.resize(1200, 800)
+    window.show()
+
+    sys.exit(app.exec())
